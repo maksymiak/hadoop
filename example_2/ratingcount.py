9da012fbc5f601a8264dab2582deacfe60c5d1d9
maksymiak <61247352+maksymiak@users.noreply.github.com>
2020-10-26 22:02:14 +0100

    Update ratingcount.py

--- example_2/ratingcount.py
+++ example_2/ratingcount.py
@@ -5,7 +5,21 @@ class MRHotelRaitingCount(MRJob):
     def mapper(self, _, line):
         (HName, HStar, HRooms, UCountry, NrReviews, rating, StayPeriod, TType, Pool, Gym, TCourt, Spa, Casino, Internet, UContinent, ReviewMonth, ReviewDay) = line.split("\t")
     
-        yield (HName, (rating))
+        yield (HName, int(rating))
+        
+    def _reducer_combiner(self, HName, rating):
+        avg, count = 0, 0
+        for tmp, c in rating:
+            avg = (avg * count + tmp * c) / (count + c)
+            count += c
+        return (HName, (avg, count))
 
+    def combiner(self, HName, rating):
+        yield self._reducer_combiner(HName, rating)
+
+    def reducer(self, HName, rating):
+        HName, (avg, count) = self._reducer_combiner(HName, rating)
+        yield (HName, avg)
+        
 if __name__ == '__main__':
     MRHotelRaitingCount.run()
